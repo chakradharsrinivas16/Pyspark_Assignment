354744a22c131b1d3887f0c14c72fcb56991f3d2
chakradharsrinivas16 <123494344+chakradharsrinivas16@users.noreply.github.com>
2023-04-17 15:06:01 +0530

    Update dataframe.py

--- dataframe.py
+++ dataframe.py
@@ -1,42 +1,71 @@
-# importing required libraries
-import requests
-from pyspark.sql.functions import col
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from config import KEY # Importing key from config file(Hiding the key)
-from pyspark.sql.functions import regexp_replace
-from pyspark.sql.functions import col
+# Importing the required imports
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+from flask import Flask, jsonify
+from dataframe import df # Fecthing the dataframe created in other file.
+from pyspark.sql.functions import sum
+ 
+print("Below is the data after cleaning and creation of dataframe :-\n")
+df.show(36) # Displaying the above fetched dataframe
+app = Flask(__name__) # creating a app from flask
+@app.route('/') # defing the things to happen on home path
+def home():
+    # returning the jsonfied index
+    return jsonify({'/most_affected_state': "Most affected state among all the states ( total death/total covid cases)",
+                    '/least_affected_state': "Least affected state among all the states ( total death/total covid cases)",
+                    '/highest_covid_cases': "State with highest covid cases.",
+                    '/least_covid_cases': "State with least covid cases.",
+                    '/total_cases': "Total cases.",
+                    '/most_efficient_state':"State that handled the covid most efficiently( total recovery/ total covid cases).",
+                    '/least_efficient_state': "State that handled the covid least efficiently( total recovery/ total covid cases).",
+                    '/getcsvfile':"To export data to csv file at given path"
+                    })
 
-# Return : response Object
-# function to get the data from given api, and return the response object
-def get_data():
-	url = "https://covid-19-india2.p.rapidapi.com/details.php"
-	headers = {
-		"X-RapidAPI-Key": KEY,
-		"X-RapidAPI-Host": "covid-19-india2.p.rapidapi.com"
-	}
-	response = requests.request("GET", url, headers=headers)
-	return response
+@app.route('/getcsvfile') # defing the things to happen on /getcsvfile path
+def getcsvfile():
+    df.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("/Users/chakradhar/Desktop/results")
+    return jsonify({"Message":"Results stored succesfully to '/Users/chakradhar/Desktop/results' path"})
+@app.route('/most_affected_state') # defing the things to happen on /most_affected_state
+def get_most_affected_state():
+    # Sorting the data frame as per given criteria in descending and selecting the top most record and state column
+    most_affected_state=df.sort((df.death/df.confirm).desc()).select(col("state")).collect()[0][0]
+    return jsonify({'most_affected_state': most_affected_state})  # returning the jsonfied response
 
-# Return : cleaned dataframe
-# Fucntion to clean the given dataframe, clenaing involves removing corrupted records, empty, void records,
-#  and striping the unwanted parts in staten names
+@app.route('/least_affected_state') # defing the things to happen on /least_affected_state
+def get_least_affected_state():
+    # Sorting the data frame as per given criteria in ascending and selecting the top most record and state column
+    least_affected_state=df.sort((df.death/df.confirm)).select(col("state")).collect()[0][0]
+    return jsonify({'least_affected_state': least_affected_state}) # returning the jsonfied response
 
-def clean_data(df):
-    df=df.drop('_corrupt_record') # droping the _corrupt_record column
-    df=df.where((df.state.isNotNull()) & (df.state!='')) #fetching records only whose state is not void or null.
-    # Casting the columns to desired formats
-    df = df.withColumn("confirm",col("confirm").cast("Long")).withColumn("cured",col("cured").cast("Long")).withColumn("death",col("death").cast("Long"))
-    # Reaaranging the columns to give a good view
-    df=df.select("slno","state","confirm","cured","death","total")
-    # Striping state names who has * st their ends
-    df=df.withColumn('state', regexp_replace('state', '\*',""))
-    return df # returning the cleaned dataframe
+@app.route('/highest_covid_cases') # defing the things to happen on /highest_covid_cases
+def get_highest_covid_cases():
+    # Sorting the data frame as per given criteria in descneding and selecting the top most record and state column
+    highest_covid_cases=df.sort((df.confirm).desc()).select(col("state")).collect()[0][0]
+    return jsonify({'get_highest_covid_cases':highest_covid_cases}) # returning the jsonfied response
 
-spark = SparkSession.builder.master('local[*]').getOrCreate() # Creating a spark session
-sc = SparkContext.getOrCreate() # creating a Spark context
-sc.setLogLevel("ERROR")
-response=get_data() # getting the response objet from the above created method
-json_rdd = sc.parallelize(response.json().values()) # creating a rdd from the data fetched above
-df = spark.read.json(json_rdd) # creating the dataframe for the above created rdd
-df=clean_data(df) # cleaning the above created data frame
+@app.route('/least_covid_cases') # defing the things to happen on /least_covid_cases
+def get_least_covid_cases():
+    # Sorting the data frame as per given criteria in acending and selecting the top most record and state column
+    least_covid_cases=df.sort(df.confirm).select(col("state")).collect()[0][0]
+    return jsonify({'get_least_covid_cases':least_covid_cases}) # returning the jsonfied response
+
+@app.route('/total_cases') # defing the things to happen on /total_cases
+def get_total_cases():
+    # Suming the data frame as per given criteria and selecting the sum.
+    total_cases=df.select(sum(df.confirm).alias("Total cases")).collect()[0][0]
+    return jsonify({'Total Cases':total_cases}) # returning the jsonfied response
+    
+@app.route('/most_efficient_state') # defing the things to happen on /most_efficient_state
+def get_most_efficient_state():
+    # Sorting the data frame as per given criteria in descending and selecting the top most record and state column
+    most_efficient_state=df.sort((df.cured/df.confirm).desc()).select(col("state")).collect()[0][0]
+    return jsonify({'most efficient_state':most_efficient_state}) # returning the jsonfied response
+
+@app.route('/least_efficient_state') # defing the things to happen on /least_efficient_state
+def get_least_efficient_state():
+    # Sorting the data frame as per given criteria in ascending and selecting the top most record and state column
+    least_efficient_state=df.sort((df.cured/df.confirm).asc()).select(col("state")).collect()[0][0]
+    return jsonify({'least efficient_state':least_efficient_state}) # returning the jsonfied response
+
+if __name__ == '__main__':
+    app.run(debug=True) #running the app
