88d150772b93a071e00fc95798bb76cf4eead3cc
chakradharsrinivas16 <123494344+chakradharsrinivas16@users.noreply.github.com>
2023-04-17 17:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from pyspark.sql.functions import *
 from flask import Flask, jsonify
 from dataframe import df # Fecthing the dataframe created in other file.
 from pyspark.sql.functions import sum
+from datetime import datetime
+
  
 print("Below is the data after cleaning and creation of dataframe :-\n")
 df.show(36) # Displaying the above fetched dataframe
@@ -23,32 +25,31 @@ def home():
 
 @app.route('/getcsvfile') # defing the things to happen on /getcsvfile path
 def getcsvfile():
-    # Exporting the data into csv as single file
-    df.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("/Users/chakradhar/Desktop/results")
-    return jsonify({"Message":"Results stored succesfully to '/Users/chakradhar/Desktop/results' path"}) # returning the success meassage jsonfied response
-  
+    df.repartition(1).write.format("com.databricks.spark.csv").option("header", "true").save("/Users/chakradhar/Desktop/results "+str(datetime.now()))
+    return jsonify({"Message":"Results stored succesfully to '/Users/chakradhar/Desktop/results"+str(datetime.now())+"' path"})
+
 @app.route('/most_affected_state') # defing the things to happen on /most_affected_state
 def get_most_affected_state():
     # Sorting the data frame as per given criteria in descending and selecting the top most record and state column
-    most_affected_state=df.sort((df.death.cast("Long")/df.confirm.cast("Long")).desc()).select(col("state")).collect()[0][0]
+    most_affected_state=df.sort((df.death/df.confirm).desc()).select(col("state")).collect()[0][0]
     return jsonify({'most_affected_state': most_affected_state})  # returning the jsonfied response
 
 @app.route('/least_affected_state') # defing the things to happen on /least_affected_state
 def get_least_affected_state():
     # Sorting the data frame as per given criteria in ascending and selecting the top most record and state column
-    least_affected_state=df.sort((df.death.cast("Long")/df.confirm.cast("Long"))).select(col("state")).collect()[0][0]
+    least_affected_state=df.sort((df.death/df.confirm)).select(col("state")).collect()[0][0]
     return jsonify({'least_affected_state': least_affected_state}) # returning the jsonfied response
 
 @app.route('/highest_covid_cases') # defing the things to happen on /highest_covid_cases
 def get_highest_covid_cases():
     # Sorting the data frame as per given criteria in descneding and selecting the top most record and state column
-    highest_covid_cases=df.sort((df.confirm).cast("Long").desc()).select(col("state")).collect()[0][0]
+    highest_covid_cases=df.sort((df.confirm).desc()).select(col("state")).collect()[0][0]
     return jsonify({'get_highest_covid_cases':highest_covid_cases}) # returning the jsonfied response
 
 @app.route('/least_covid_cases') # defing the things to happen on /least_covid_cases
 def get_least_covid_cases():
     # Sorting the data frame as per given criteria in acending and selecting the top most record and state column
-    least_covid_cases=df.sort(df.confirm.cast("Long")).select(col("state")).collect()[0][0]
+    least_covid_cases=df.sort(df.confirm).select(col("state")).collect()[0][0]
     return jsonify({'get_least_covid_cases':least_covid_cases}) # returning the jsonfied response
 
 @app.route('/total_cases') # defing the things to happen on /total_cases
@@ -60,13 +61,13 @@ def get_total_cases():
 @app.route('/most_efficient_state') # defing the things to happen on /most_efficient_state
 def get_most_efficient_state():
     # Sorting the data frame as per given criteria in descending and selecting the top most record and state column
-    most_efficient_state=df.sort((df.cured.cast("Long")/df.confirm.cast("Long")).desc()).select(col("state")).collect()[0][0]
+    most_efficient_state=df.sort((df.cured/df.confirm).desc()).select(col("state")).collect()[0][0]
     return jsonify({'most efficient_state':most_efficient_state}) # returning the jsonfied response
 
 @app.route('/least_efficient_state') # defing the things to happen on /least_efficient_state
 def get_least_efficient_state():
     # Sorting the data frame as per given criteria in ascending and selecting the top most record and state column
-    least_efficient_state=df.sort((df.cured.cast("Long")/df.confirm.cast("Long")).asc()).select(col("state")).collect()[0][0]
+    least_efficient_state=df.sort((df.cured/df.confirm).asc()).select(col("state")).collect()[0][0]
     return jsonify({'least efficient_state':least_efficient_state}) # returning the jsonfied response
 
 if __name__ == '__main__':
